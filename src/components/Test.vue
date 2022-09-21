23cd820a32f8522bafb5009537d42fb67915ac44
Zhiwu Mao <zwmao@outlook.com>
2022-09-21 15:37:03 +0800

    first commit

---
+++ src/components/Test.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { Ref, ref } from 'vue'
+const count = ref(0)
+const add = function () {
+  count.value++
+}
+</script>
+
+<template>
+  <h1>{{ count }}</h1>
+  <button @click="add">加1</button>
+</template>
+
+<style scoped></style>
